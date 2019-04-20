a38fb787ec04d7b0aedaee7f9a748cedbb62a49f
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-04-20 18:56:19 +0200

    Added Proc File and Created DB Connection to Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mini Project 3
                 </div>
 
                 <div class="links">
